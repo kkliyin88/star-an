203214eecab6557179c2e82a3a99e9d14fe29bd9
Leein <leein1986@foxmail.com>
2019-11-25 08:40:10 +0800

    李铟提交

---
+++ src/page/products/levelOne/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class='wrap'>
+	  <div class='container1'>
+		<section class='pic'>
+			<ul >
+				<li v-for='item in picArr1' class='pointer'>
+					<router-link :to='item.routerPath'>
+					<div class='img-pic' @onmouseover.native='changeImg(item)'>
+						<img :src='item.piclink' />
+					</div>
+					</router-link>
+				</li>
+			</ul>
+		</section>
+		<section class='pic'>
+			<ul >
+				<li v-for='item in picArr2' class='pointer'>
+					<router-link :to='item.routerPath'>
+					<div class='img-pic' @onmouseover.native='changeImg(item)'>
+						<img :src='item.piclink' />
+					</div>
+					</router-link>
+				</li>
+			</ul>
+		</section>
+	  </div>
+  </div>
+</template>
+<script>
+    export default {
+      name: 'levelOne',
+      components:{
+
+      },
+      data () {
+        return {
+			picArr1:[
+				{
+					piclink:'/static/image/products/levelOne/',
+					routerPath:{path:'/products/levelTwo',query:{name:'sensor',id:1}},
+				},
+				{
+					piclink:'/static/image/products/levelOne/',
+					routerPath:{path:'/products/levelTwo',query:{name:'controller',id:2}},
+				},
+				{
+					piclink:'/static/image/products/levelOne/',
+					routerPath:{path:'/products/levelTwo',query:{name:'driver',id:3}},
+				},
+				
+			],
+			picArr2:[
+				{
+					piclink:'/static/image/products/levelOne/',
+					routerPath:{path:'/products/levelTwo',query:{name:'sensor',id:4}},
+				},
+				{
+					piclink:'/static/image/products/levelOne/',
+					routerPath:{path:'/products/levelTwo',query:{name:'controller',id:5}},
+				},
+				{
+					piclink:'/static/image/products/levelOne/',
+					routerPath:{path:'/products/levelTwo',query:{name:'driver',id:6}},
+				},
+				
+			],
+			
+        }
+      },
+      mounted(){
+		   this.operatorData(this.picArr1,1);
+		   this.operatorData(this.picArr2,2)
+      },
+      methods:{
+		 operatorData(arr,i){
+			 arr.map((item,index)=>{
+				 item.piclink  = item.piclink + this.$route.query.name +'/' +i+'-'+ (index+1)+'.png';
+			 })
+		 }
+      }
+    }
+</script>
+<style scoped lang='less'>
+	@import "./index.less";
+</style>
---
+++ src/page/products/levelTwo/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class='wrap'>
+	  <div class='container1'>
+		   <section class="left">
+			   <div class="main_pic">
+				   <img :src='mainPic' />
+			   </div>
+			   <div class="small_pic">
+			   		<ul>
+						<li @click='selectPic(index)' v-for="(item,index) in smallPicArr">
+							<img :src='item.pic' />
+						</li>
+					</ul>		   
+			   </div>
+		   </section>
+		   <section class="right">22</section>
+	  </div>
+  </div>
+</template>
+<script>
+    export default {
+      name: 'levelTwo',
+      components:{
+		  
+
+      },
+      data () {
+        return {
+			
+			mainPic:'/static/image/products/levelTwo/',
+			basePath:'/static/image/products/levelTwo/',
+			smallPicArr:
+			[
+				{pic:'/static/image/products/levelTwo/'},
+				{pic:'/static/image/products/levelTwo/'},
+				{pic:'/static/image/products/levelTwo/'},
+				{pic:'/static/image/products/levelTwo/'},
+				{pic:'/static/image/products/levelTwo/'},
+			]
+			
+			
+        }
+      },
+      mounted(){
+		console.log('router',this.$route.query);
+		this.operator(1);
+      },
+
+      methods:{
+		 operator(i){
+			 this.mainPic = this.mainPic + this.$route.query.name +'/' +i+'.png';
+			 this.smallPicArr.map((item,index)=>{
+				 item.pic = item.pic + this.$route.query.name + '/' + (index+1) + '.png'
+			 })
+			
+		 },
+		 selectPic(i){
+			 console.log('i',i)
+			 console.log('i',i)
+			 this.mainPic = this.smallPicArr[i].pic 
+		 }
+
+      }
+    }
+</script>
+<style scoped lang='less'>
+	@import "./index.less";
+</style>
